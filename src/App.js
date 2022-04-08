577ccc076030795e344c9c782be6c5710a3d8531
ThunderKlappe <timschley@gmail.com>
2022-04-07 20:09:17 -0700

    refactored the drag and drop sections into their own component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import AddButton from "./components/AddButton";
 import NewTask from "./components/NewTask";
 import Task from "./components/Task";
 import TaskHeader from "./components/TaskHeader";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { swap } from "./actions";
+import MyDragDrop from "./components/MyDragDrop";
 
 function App() {
-    const dispatch = useDispatch();
     const showInput = useSelector(state => state.showInput);
     const tasks = useSelector(state => state.tasks);
     const trueTasks = tasks.map(x => x);
-
-    // trueTasks.push({ id: "red", priority: "Low", taskName: "Task2", complete: false });
-    // trueTasks.push({ id: "blue", priority: "Low", taskName: "Task", complete: false });
 
     let incompleteTasks = [];
     let completeTasks = [];
@@ -30,12 +25,6 @@ function App() {
         }
     });
 
-    function onDragEnd(results) {
-        if (!results.destination) return;
-        const sourceIndex = results.source.index;
-        const destinationIndex = results.destination.index;
-        dispatch(swap({ sourceIndex, destinationIndex }));
-    }
     return (
         <Container id="content">
             <Row className="justify-content-md-center">
@@ -45,68 +34,9 @@ function App() {
             </Row>
             <Row>{showInput ? <NewTask /> : ""}</Row>
             <Row>{tasks.length > 0 ? <TaskHeader /> : ""}</Row>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable type="incomplete" droppableId="incomplete">
-                    {provided => {
-                        return (
-                            <div className="incomplete" {...provided.droppableProps} ref={provided.innerRef}>
-                                {incompleteTasks.map((task, index) => {
-                                    return (
-                                        <Draggable
-                                            type="incomplete"
-                                            key={`key-${index}`}
-                                            draggableId={`id-${index}`}
-                                            index={index}
-                                        >
-                                            {provided => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                >
-                                                    <Row>{task}</Row>
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </div>
-                        );
-                    }}
-                </Droppable>
-            </DragDropContext>
-            <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable type="complete" droppableId="complete">
-                    {provided => {
-                        return (
-                            <div className="complete" {...provided.droppableProps} ref={provided.innerRef}>
-                                {completeTasks.map((task, index) => {
-                                    return (
-                                        <Draggable
-                                            type="complete"
-                                            key={`key-${index}`}
-                                            draggableId={`id-${index}`}
-                                            index={index}
-                                        >
-                                            {provided => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                >
-                                                    <Row>{task}</Row>
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </div>
-                        );
-                    }}
-                </Droppable>
-            </DragDropContext>
+
+            <MyDragDrop type="incomplete" draggables={incompleteTasks} />
+            <MyDragDrop type="complete" draggables={completeTasks} />
         </Container>
     );
 }
---
+++ src/components/MyDragDrop.js
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Row } from "react-bootstrap";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { swap } from "../actions";
+
+function MyDragDrop(props) {
+    const dispatch = useDispatch();
+    function onDragEnd(results) {
+        if (!results.destination) return;
+        const sourceIndex = results.source.index;
+        const destinationIndex = results.destination.index;
+        dispatch(swap({ sourceIndex, destinationIndex }));
+    }
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable type={props.type} droppableId={props.type}>
+                {provided => {
+                    return (
+                        <div className={props.type} {...provided.droppableProps} ref={provided.innerRef}>
+                            {props.draggables.map((task, index) => {
+                                return (
+                                    <Draggable
+                                        type={props.type}
+                                        key={`key-${index}`}
+                                        draggableId={`id-${index}`}
+                                        index={index}
+                                    >
+                                        {provided => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <Row>{task}</Row>
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                );
+                            })}
+                            {provided.placeholder}
+                        </div>
+                    );
+                }}
+            </Droppable>
+        </DragDropContext>
+    );
+}
+export default MyDragDrop;
